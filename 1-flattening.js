fcdd7283562974f1feb4126cf056ebfcbe3ce963
Daceit <matthew.sullivan23@student.cart.org>
2024-02-15 15:23:54 -0800

    asd

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,8 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-
-
+function reduce(array) {
+    const conarray = array.reduce((a,b) => a.concat(b))
+    return conarray
+}
+console.log(reduce(arrays))
 // → [1, 2, 3, 4, 5, 6]
--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,6 +1,12 @@
 // Your code here.
+function loop(num,test,change,output){
+    for(n = num;test;change){
+        output(n)
+    }
+}
 
 loop(3, n => n > 0, n => n - 1, console.log);
+
 // → 3
 // → 2
 // → 1
