ee9d265826041012018be82a287116f9598bf100
Daceit <matthew.sullivan23@student.cart.org>
2024-02-15 15:48:39 -0800

    asdasd

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,8 +1,13 @@
 // Your code here.
 function loop(num,test,change,output){
-    for(n = num;test;change){
-        output(n)
-    }
+    const n = num
+   if (test(n) === false){
+      output(n)
+   } else if (test(n) === true) {
+    output(n)
+    change(n);
+    num = n
+   }
 }
 
 loop(3, n => n > 0, n => n - 1, console.log);
